22725b289646d341bac390e3b3f223457b79bb97
Stefan <stefandeyoung@gmail.com>
2017-02-13 21:35:04 -0500

    fixed lat/lon order in api call. centered on Ottawa.

--- sketch.js
+++ sketch.js
@@ -3,25 +3,29 @@
 
 var mapimg;
 
-var clat = 0;
-var clon = 0;
+//Ottawa Latitude: 45.4215 N. Longitude: 75.6972 W
+//var clat = 0;
+//var clon = 0;
+var clat = 45.4215;
+var clon = -75.6972;
 
 var ww = 1024;
 var hh = 512;
 
-var zoom = 1;
-var earthquakes;
+var zoom = 5;
+//var earthquakes;
 
 function preload() {
   mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' +
-    clat + ',' + clon + ',' + zoom + '/' +
+    clon + ',' + clat + ',' + zoom + '/' +
     ww + 'x' + hh +
     '?access_token=pk.eyJ1Ijoic3RlZmFuZHkiLCJhIjoiY2l6NHcxNW96MDU0ZTJ3cDhmZHZzbTJnbiJ9.zhbVCkE0TCIC01rVngePTg');
   // earthquakes = loadStrings('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv');
-  earthquakes = loadStrings('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv');
+  //earthquakes = loadStrings('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv');
 }
 
 function mercX(lon) {
+  //Mercator projection of longitude to X
   lon = radians(lon);
   var a = (256 / PI) * pow(2, zoom);
   var b = lon + PI;
@@ -29,6 +33,7 @@ function mercX(lon) {
 }
 
 function mercY(lat) {
+  //Mercator projection of latitude to Y
   lat = radians(lat);
   var a = (256 / PI) * pow(2, zoom);
   var b = tan(PI / 4 + lat / 2);
@@ -47,21 +52,25 @@ function setup() {
   var cx = mercX(clon);
   var cy = mercY(clat);
 
-  for (var i = 1; i < earthquakes.length; i++) {
-    var data = earthquakes[i].split(/,/);
-    //console.log(data);
-    var lat = data[1];
-    var lon = data[2];
-    var mag = data[4];
-    var x = mercX(lon) - cx;
-    var y = mercY(lat) - cy;
-    mag = pow(10, mag);
-    mag = sqrt(mag);
-    var magmax = sqrt(pow(10, 10));
-    var d = map(mag, 0, magmax, 0, 180);
-    stroke(255, 0, 255);
-    fill(255, 0, 255, 200);
-    ellipse(x, y, d, d);
-  }
+
+
+  ellipse()
+
+  // for (var i = 1; i < earthquakes.length; i++) {
+  //   var data = earthquakes[i].split(/,/);
+  //   //console.log(data);
+  //   var lat = data[1];
+  //   var lon = data[2];
+  //   var mag = data[4];
+  //   var x = mercX(lon) - cx;
+  //   var y = mercY(lat) - cy;
+  //   mag = pow(10, mag);
+  //   mag = sqrt(mag);
+  //   var magmax = sqrt(pow(10, 10));
+  //   var d = map(mag, 0, magmax, 0, 180);
+  //   stroke(255, 0, 255);
+  //   fill(255, 0, 255, 200);
+  //   ellipse(x, y, d, d);
+  // }
 
 }
